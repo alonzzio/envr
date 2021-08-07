c1c5770ae929029ffb3af2007d264c41c49aa3c7
alonzzio <57526912+alonzzio@users.noreply.github.com>
2021-08-07 14:43:40 +0930

    multiple file try

--- envr/loadfile.go
+++ envr/loadfile.go
@@ -7,13 +7,21 @@ import (
 )
 
 // LoadEnv loads env file from directory. Add env file containing folder and file name
-func LoadEnv(envDirectory string, filename ...string) error {
+func LoadEnv(envDirectory string, filenames ...string) error {
 	if len(envDirectory) < 1 {
 		return errors.New("environment directory not supplied")
 	}
 
+	var f []string
+	for _, file := range filenames {
+		file = envDirectory + "/" + file // building the directory path
+		f = append(f, file)
+		fmt.Println(file)
+	}
+
 	// loads environment files from  director
-	err := godotenv.Load(fmt.Sprintf("%v/%v", envDirectory, filename))
+	err := godotenv.Load(f...)
+	//err := godotenv.Load(fmt.Sprintf("%v/%v", envDirectory, filename))
 	if err != nil {
 		return err
 	}
